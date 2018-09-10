ef4b4797f557af51862536f00dbda4336b68d08b
Raekye <Raekye@users.noreply.github.com>
2018-09-10 10:45:48 -0400

    god knows how long this was

--- siyu/src/global.h
+++ siyu/src/global.h
@@ -2,6 +2,8 @@
 #define SIYU_GLOBAL_H_INCLUDED
 
 #include <cstdint>
+#include <cstddef>
+#include <type_traits>
 
 typedef int8_t Byte;
 typedef uint8_t UByte;
@@ -11,5 +13,15 @@ typedef int32_t Int;
 typedef uint32_t UInt;
 typedef int64_t Long;
 typedef uint64_t ULong;
+typedef std::size_t SizeT;
+
+static_assert(sizeof(Byte) == 1, "sanity");
+static_assert(sizeof(Short) == 2, "sanity");
+static_assert(sizeof(Int) == 4, "sanity");
+static_assert(sizeof(Long) == 8, "sanity");
+static_assert(sizeof(SizeT) == 8, "sanity");
+static_assert(std::is_same<ULong, SizeT>::value, "sanity");
+static_assert(!std::is_same<Long, SizeT>::value, "sanity");
+static_assert(std::is_same<int, Int>::value, "sanity");
 
 #endif /* SIYU_GLOBAL_H_INCLUDED */
